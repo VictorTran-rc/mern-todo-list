d092aee59212c7debb8a8417868a084239cf564a
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-05-25 17:06:53 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreateToDo from "./components/create-todo";
+import EditToDo from "./components/edit-toDo";
+import ToDoList from "./components/toDolist";
+import logo from "./logo.jpg";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+  <div className="container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+        <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+      </a>
+      <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+      <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">Todos</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/create" className="nav-link">Create Todo</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <br/>
+    <Route path="/" exact component={ToDoList} />
+    <Route path="/edit/:id" component={EditToDo} />
+    <Route path="/create" component={CreateToDo} />
+  </div>
+</Router>
+
+      );
+}
+}
+export default App;
